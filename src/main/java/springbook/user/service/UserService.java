b58f3c0efc57a431f80b3d95f713bb53db062678
leeikjeun <dldlrwjs12@gmail.com>
2019-03-14 15:12:24 +0900

    기술과 환경에 종속되는 트랜잭션 해결 방안인데 JTA 를 사용하는 방법이 완벽하지는 않다 (UserTransaction 이란것이 없다 아마 실제서비스에서 만들어서 사용하는 방식인듯)

--- src/main/java/springbook/user/service/UserService.java
+++ src/main/java/springbook/user/service/UserService.java
@@ -7,6 +7,8 @@ import springbook.user.domain.Level;
 import springbook.user.domain.User;
 
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -14,12 +16,19 @@ import java.util.List;
 
 /**
  * Created by adaeng on 11/03/2019.
+ * 현재 밑의 트랜잭션 경계설정 코드는 기술과 환경에 종속된다.
+ * 만약 여러개의 DB에 데이터를 넣은 작업을 해야한다면 한개 이상의 DB로의 작업을 하나의 트랜잭션으로 만드는건 불가능
+ * 왜냐하면 JDBC의 Connection을 이용한 트랜잭션 방식은 로컬 트랜잭션이기 때문에
+ * 로컬 트랜잭션은 하나의 DB 케넥션에 종속된다
+ * 따라서 각 DB에 커넥션을 통해 만드는 것이 아니라 트랜잭션 관리자를 통해 글로벌 트랜잭션방식을 사용해야 한다.
+ * 자바에서 제공하는 API JTA
  */
 public class UserService {
 
     private UserDao userDao;
     private UserLevelUpgradePolicy userLevelUpgradePolicy;
     private DataSource dataSource;
+
 
     public void setDataSource(DataSource dataSource) {
         this.dataSource = dataSource;
@@ -33,8 +42,10 @@ public class UserService {
         this.userLevelUpgradePolicy = userLevelUpgradePolicy;
     }
 
-    public void upgradeLevels() throws SQLException {
-        TransactionSynchronizationManager.initSynchronization();
+    public void upgradeLevels() throws SQLException, NamingException {
+        InitialContext ctx = new InitialContext();
+        UserTransaction tx = (UserTransaction)ctx.lookup("USER_TX_JNDI_NAME");
+
         Connection c = DataSourceUtils.getConnection(dataSource);
         c.setAutoCommit(false);
 
