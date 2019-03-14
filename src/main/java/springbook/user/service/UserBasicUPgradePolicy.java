2784fba76d06fb3f2e08eef3b75044623bff9f9f
leeikjeun <dldlrwjs12@gmail.com>
2019-03-14 15:43:02 +0900

    메일 서비스 추가

--- src/main/java/springbook/user/service/UserBasicUPgradePolicy.java
+++ src/main/java/springbook/user/service/UserBasicUPgradePolicy.java
@@ -1,8 +1,17 @@
 package springbook.user.service;
 
+import org.springframework.mail.MailSender;
 import springbook.user.dao.UserDao;
 import springbook.user.domain.Level;
 import springbook.user.domain.User;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
 
 /**
  * Created by adaeng on 13/03/2019.
@@ -35,5 +44,25 @@ public class UserBasicUPgradePolicy implements UserLevelUpgradePolicy {
     public void upgradeLevel(User user) {
         user.upgradeLevel();
         userDao.update(user);
+        sendUpgradeEmail(user);
+    }
+
+    private void sendUpgradeEmail(User user) {
+        Properties props = new Properties();
+        props.put("mail.smtp.host","mail.ksug.org");
+        Session s = Session.getInstance(props, null);
+
+        MimeMessage message = new MimeMessage(s);
+
+        try {
+            message.setFrom(new InternetAddress("useradmin@ksung.org"));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(user.getId()));
+            message.setSubject("Upgrade 안내");
+            message.setText("사용자님의 등급이 " + user.getLevel().name() + "로 업그레이드 되었습니다");
+            Transport.send(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+
     }
 }
